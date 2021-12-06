ee4c6bec93ce183b6280be4cf14ffa6797dde518
Torben Carstens <git@carstens.tech>
2021-12-06 20:17:56 +0100

    Search for movies by title

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import re
+
 from fastapi import FastAPI, HTTPException
 from imdb import IMDb
 from imdb.Movie import Movie
 from imdb.helpers import get_byURL
 from pydantic import BaseModel
 
-import re
-
 
 class ResolverRequest(BaseModel):
     imdbUrl: str
 
 
+class SearchRequest(BaseModel):
+    title: str
+
+
 app = FastAPI()
+
+COVER_URL_SIZE_REGEX = r"._V\d+_\w+\d+_\w+\d+,\d+,(\d+),(\d+)_"
 
 
 def remove_size_from_cover_url(url: str) -> str:
     pattern = r"._V\d+_\w+\d+_\w+\d+,\d+,\d+,\d+_"
     return re.sub(pattern, "", url)
+
+
+@app.post("/search")
+def search(req: SearchRequest):
+    imdb = IMDb()
+
+    data = [{"title": movie.data['title'], "url": f"https://www.imdb.com/title/tt{movie.movieID}"} for movie in
+            imdb.search_movie(req.title)]
+    return {
+        "results": data
+    }
 
 
 @app.post("/")
