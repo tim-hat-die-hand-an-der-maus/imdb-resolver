f5f7913d422ae03af39c65675473db1d6b407b9b
Torben Carstens <git@carstens.tech>
2023-02-18 20:36:41 +0100

    Add MovieResponse struct
    Closes #68

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import re
-from typing import Dict
+from typing import Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import HTTPException, FastAPI
 from imdb import Cinemagoer
 from imdb.Movie import Movie
 from imdb.helpers import get_byURL
 from pydantic import BaseModel
-
-
-class ResolverRequest(BaseModel):
-    imdbUrl: str
-
-
-class SearchRequest(BaseModel):
-    title: str
-
-
-app = FastAPI()
 
 COVER_URL_SIZE_REGEX = r"._V\d+_\w+\d+_\w+\d+,\d+,(\d+),(\d+)_"
 
@@ -32,22 +21,44 @@ def get_ratio_from_cover_url(url: str) -> float:
         return int(height) / int(width)
 
 
-def movie_to_json(movie) -> Dict:
-    cover_url = movie.data["cover url"]
-    cover_ratio = get_ratio_from_cover_url(cover_url)
-    cover_url = remove_size_from_cover_url(cover_url)
+class ResolverRequest(BaseModel):
+    imdbUrl: str
 
-    return {
-        "id": movie.movieID,
-        "title": movie.data['title'],
-        "year": movie.data.get('year'),
-        "rating": str(movie.data.get('rating')),
-        "cover": {
-            "url": cover_url,
-            "ratio": cover_ratio,
-        },
-        "coverUrl": cover_url,
-    }
+
+class SearchRequest(BaseModel):
+    title: str
+
+
+class CoverMetadataResponse(BaseModel):
+    url: str
+    ratio: float
+
+
+class MovieResponse(BaseModel):
+    id: str
+    title: str
+    year: int
+    rating: str
+    cover: CoverMetadataResponse
+    coverUrl: str
+
+    @classmethod
+    def from_imdb_movie(cls, movie: Movie):
+        cover_url = movie.data["cover url"]
+        cover_ratio = get_ratio_from_cover_url(cover_url)
+        cover_url = remove_size_from_cover_url(cover_url)
+
+        return cls(
+            id=movie.movieID,
+            title=movie.data['title'],
+            year=movie.data.get('year'),
+            rating=str(movie.data.get('rating')),
+            cover=CoverMetadataResponse(url=cover_url, ratio=cover_ratio),
+            coverUrl=cover_url,
+        )
+
+
+app = FastAPI()
 
 
 @app.post("/search")
@@ -55,22 +66,28 @@ def search(req: SearchRequest):
     imdb = Cinemagoer()
 
     return {
-        "results": [movie_to_json(movie) for movie in imdb.search_movie(req.title)]
+        "results": [MovieResponse.from_imdb_movie(movie).dict() for movie in imdb.search_movie(req.title)]
     }
+
+
+def resolve_link(url: str) -> Optional[MovieResponse]:
+    movie = get_byURL(url)
+
+    if not movie:
+        match = re.search(r".*tt(\d+)", url)
+
+        if match:
+            imdb_id = match.group(1)
+            return Cinemagoer().get_movie(imdb_id)
+
+    return None
 
 
 @app.post("/")
 def movie_by_link(req: ResolverRequest):
-    movie = get_byURL(req.imdbUrl)
+    movie = resolve_link(req.imdbUrl)
 
     if not movie:
-        match = re.search(r".*tt(\d+)", req.imdbUrl)
-
-        if match:
-            imdb_id = match.group(1)
-            movie = Cinemagoer().get_movie(imdb_id)
-
-    if not isinstance(movie, Movie):
         raise HTTPException(status_code=404, detail=f"link (`{req.imdbUrl}`) couldn't be resolved to a movie")
     else:
-        return movie_to_json(movie)
+        return movie
